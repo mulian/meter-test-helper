5457aa0563d78fe73eda531e7b08fbccc4275d50
mulian <Max.Hoffmann@tu-bs.de>
2016-12-03 17:22:29 +0100

    add package desc.

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'mulian:test-helper',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Test helper for SecureLayer.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/mulian/meter-test-helper',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
